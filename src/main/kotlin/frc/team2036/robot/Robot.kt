56111bb76fffbd18c3ad0669ae881461bcb22e37
Saurabh Totey <SaurabhTotey@gmail.com>
2018-06-07 14:04:54 -0600

    Initial commit

---
+++ src/main/kotlin/frc/team2036/robot/Robot.kt
@@ -0,0 +1,23 @@
+package frc.team2036.robot
+
+import edu.wpi.first.wpilibj.IterativeRobot
+
+class Robot : IterativeRobot() {
+    override fun robotInit() {}
+
+    override fun disabledInit() {}
+
+    override fun autonomousInit() {}
+
+    override fun teleopInit() {}
+
+    override fun testInit() {}
+
+    override fun disabledPeriodic() {}
+
+    override fun autonomousPeriodic() {}
+
+    override fun teleopPeriodic() {}
+
+    override fun testPeriodic() {}
+}
