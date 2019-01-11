454d5c0e9ee695b015dafc046e1bcae038066859
Saurabh Totey <SaurabhTotey@gmail.com>
2019-01-10 17:12:26 -0700

    Added dependency and made mechanism test in Robot

--- src/main/kotlin/frc/team2036/robot/Robot.kt
+++ src/main/kotlin/frc/team2036/robot/Robot.kt
@@ -1,8 +1,40 @@
 package frc.team2036.robot
 
+import edu.wpi.first.wpilibj.PWMTalonSRX
 import frc.team2036.robot.knightarmor.KnightBot
+import edu.wpi.first.wpilibj.XboxController
+
+//The driverstation port where the controller is connected
+const val CONTROLLER_PORT = 0
 
 /**
- * TODO: put 2018-2019 season code here
+ * The main and only class for the DDR bot
+ * Is a timed robot which will have some of its methods periodically called
  */
-class Robot : KnightBot()
+class Robot : KnightBot() {
+
+    //The DDR mat; is of type XboxController, but doesn't really need to be an XboxController
+    lateinit var controller: XboxController
+
+    lateinit var talon1: PWMTalonSRX
+    lateinit var talon2: PWMTalonSRX
+
+    /**
+     * Initializer that sets up the controller and the drivetrain
+     */
+    override fun robotInit() {
+        this.controller = XboxController(CONTROLLER_PORT)
+        this.talon1 = PWMTalonSRX(0)
+        this.talon2 = PWMTalonSRX(3)
+    }
+
+    /**
+     * Polls the controller and then runs the robot
+     * Gets called repeatedly during the teleoperated period
+     */
+    override fun teleopPeriodic() {
+        this.talon1.speed = -this.controller.y
+        this.talon2.speed = this.controller.y
+    }
+
+}
--- src/main/kotlin/frc/team2036/robot/knightarmor/KnightController.kt
+++ src/main/kotlin/frc/team2036/robot/knightarmor/KnightController.kt
@@ -4,7 +4,7 @@ package frc.team2036.robot.knightarmor
  * A class that handles binding controls to buttons and joysticks on controllers
  * Ensures that what each button does is clear and such
  */
-open class KnightController {
+open class KnightController(port: Int) {
 
     //TODO: make
 
