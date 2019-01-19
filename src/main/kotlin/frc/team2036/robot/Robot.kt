d960bfddb048b12fddca110aac2272769901fa08
edwardwawrzynek <32946898+edwardwawrzynek@users.noreply.github.com>
2019-01-18 17:49:43 -0700

    temporary control for all parts of robot

--- src/main/kotlin/frc/team2036/robot/Robot.kt
+++ src/main/kotlin/frc/team2036/robot/Robot.kt
@@ -4,38 +4,46 @@ import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.PWMTalonSRX
 import frc.team2036.robot.knightarmor.KnightBot
 import edu.wpi.first.wpilibj.XboxController
-import edu.wpi.first.wpilibj.drive.DifferentialDrive
+import edu.wpi.first.wpilibj.drive.MecanumDrive
 
 class Robot : KnightBot() {
 
     lateinit var controller: XboxController
-    lateinit var mechanismTalon1: PWMTalonSRX
-    lateinit var mechanismTalon2: PWMTalonSRX
-    lateinit var drivetrain: DifferentialDrive
+    lateinit var drivetrain: MecanumDrive
+    lateinit var elevatorMotor: PWMTalonSRX
+    lateinit var grabMotor1: PWMTalonSRX
+    lateinit var grabMotor2: PWMTalonSRX
 
     override fun robotInit() {
-        this.controller = XboxController(0)
-        this.mechanismTalon1 = PWMTalonSRX(2)
-        this.mechanismTalon2 = PWMTalonSRX(3)
-        this.drivetrain = DifferentialDrive(PWMTalonSRX(1), PWMTalonSRX(0))
+        this.controller = XboxController(1)
+        this.drivetrain = MecanumDrive(PWMTalonSRX(0), PWMTalonSRX(1), PWMTalonSRX(2), PWMTalonSRX(3))
+        this.elevatorMotor = PWMTalonSRX(4)
+        this.grabMotor1 = PWMTalonSRX(5)
+        this.grabMotor2 = PWMTalonSRX(6)
     }
 
     override fun teleopPeriodic() {
-        this.drivetrain.arcadeDrive(-this.controller.getY(GenericHID.Hand.kLeft), this.controller.getX(GenericHID.Hand.kLeft))
+        //run drive train
+        this.drivetrain.driveCartesian(-this.controller.getY(GenericHID.Hand.kLeft) * 1.0, -this.controller.getX(GenericHID.Hand.kLeft) *1.0, this.controller.getX(GenericHID.Hand.kRight) * 1.0)
+        //run elevator
+        this.elevatorMotor.speed = this.controller.getY(GenericHID.Hand.kRight) * 1.0;
+        //this.drivetrain.driveCartesian(1.0, 0.0, 0.0);
+
+        //run intake
         when {
             this.controller.getBumper(GenericHID.Hand.kLeft) -> {
-                this.mechanismTalon1.speed = -1.0
-                this.mechanismTalon2.speed = 1.0
+                this.grabMotor1.speed = -1.0
+                this.grabMotor2.speed = 1.0
             }
             this.controller.getBumper(GenericHID.Hand.kRight) -> {
-                this.mechanismTalon1.speed = 1.0
-                this.mechanismTalon2.speed = -1.0
+                this.grabMotor1.speed = 1.0
+                this.grabMotor2.speed = -1.0
             }
             else -> {
-                this.mechanismTalon1.speed = 0.0
-                this.mechanismTalon2.speed = 0.0
+                this.grabMotor1.speed = 0.0
+                this.grabMotor2.speed = 0.0
             }
-        }
+}
     }
 
 }
